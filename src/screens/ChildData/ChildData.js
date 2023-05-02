b68ab1184aed3957f967e5844a2b8bfbea055a38
mugishap <precieuxmugisha@gmail.com>
2023-05-02 15:28:41 +0200

    [feat]: Add Child Data page

--- src/screens/ChildData/ChildData.js
+++ src/screens/ChildData/ChildData.js
@@ -21,21 +21,40 @@ const ChildData = ({ navigation }) => {
             image: require("./../../assets/images/boy.png")
         },
     ]
+
+    const ages = [
+        {
+            name: "0-4",
+            label: "0 - 4",
+        },
+        {
+            name: "4-6",
+            label: "4 - 6",
+        },
+        {
+            name: "6-10",
+            label: "6 - 10",
+        },
+        {
+            name: "11-14",
+            label: "11 - 14",
+        },
+    ]
     return (
         <View style={{ flex: 1, backgroundColor: "white" }}>
             <Image style={{}} source={require("./../../assets/images/Audio3.png")} />
             <View style={{ position: "absolute", width: "100%", height: "80%", backgroundColor: "transparent", bottom: 0 }}>
                 <View style={{ height: "25%", borderTopEndRadius: 60, borderTopStartRadius: 60, backgroundColor: "rgba(255, 255, 255, 0.8)", paddingTop: 20, opacity: 80 }}>
-                    <Text style={{ fontWeight: "bold", textAlign: "center", marginTop: 20, fontSize: 32 }}>Let's create a wonderland for your child</Text>
+                    <Text style={{ fontWeight: "bold", textAlign: "center", marginTop: 15, fontSize: 32 }}>Let's create a wonderland for your child</Text>
                 </View>
                 <View style={{ width: "100%", backgroundColor: "white", height: "75%", display: "flex", alignItems: "center" }}>
                     <TextInput
-                        style={{ height: 50, paddingLeft: 10, borderColor: '#B9B9B9', borderWidth: 1, borderRadius: 10, padding: 2, width: "90%", marginVertical: 10 }}
+                        style={{ height: 50, paddingLeft: 10, borderColor: '#B9B9B9', borderWidth: 1, borderRadius: 10, padding: 2, width: "90%", marginVertical: 0 }}
                         placeholder="Enter your kid's name"
                         onChangeText={(e) => setChildData({ ...childData, childName: e })}
                         value={childData.childName}
                     />
-                    <View style={{ display: "flex", marginTop: 20, alignItems: "center", justifyContent: "space-around", flexDirection: "row", width: "100%" }}>
+                    <View style={{ display: "flex", marginTop: 10, alignItems: "center", justifyContent: "space-around", flexDirection: "row", width: "100%" }}>
                         {
                             genders.map((gender, index) => {
                                 return (
@@ -49,8 +68,38 @@ const ChildData = ({ navigation }) => {
                             })
                         }
                     </View>
-                </View>
 
+                    <Text style={{ fontSize: 18, fontWeight: "500", marginTop: 20 }}>ChooseYour Age</Text>
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        flexWrap: 'wrap',
+                        justifyContent: 'space-between',
+                        padding: 10,
+                    }}>
+                        {
+                            ages.map((age, index) => {
+                                return (
+
+                                    <View style={{
+                                        width: '48%',
+                                        paddingVertical: 4,
+                                        borderColor: '#007FFF',
+                                        backgroundColor: childData.age === age.name ? "#007FFF" : "white",
+                                        borderWidth: 1,
+                                        marginBottom: 10,
+                                        borderRadius: 20,
+                                    }}>
+                                        <Pressable key={index} onPress={() => setChildData({ ...childData, age: age.name })}>
+                                            <Text style={{ textAlign: "center", fontSize: 18, color: childData.age === age.name ? "white" : "#007FFF", fontWeight: "500" }}>{age.label}</Text>
+                                        </Pressable>
+                                    </View>
+                                )
+                            })
+                        }
+                    </View>
+                    <Pressable onPress={() => navigation.navigate("ChildData")} style={{ backgroundColor: "white", borderWidth: 1, borderColor: "#007FFF", padding: 10, borderRadius: 25, width: 300, width: "90%", marginVertical: 20 }}><Text style={{ color: "#007FFF", fontWeight: "bold", fontSize: 20, textAlign: "center" }}>Save</Text></Pressable>
+                </View>
             </View>
         </View>
     )
